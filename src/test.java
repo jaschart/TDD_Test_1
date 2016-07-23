9db13fb2cc4d3b2bd4cbc0df561b0cb604811606
Jason Hartung <jaschart@gmail.com>
2016-07-22 21:26:44 -0400

    This is pissing me off. Commit the damned files.

---
+++ src/test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Jason on 7/22/2016.
+ */
+public class test {
+    public static void main(String[] args){
+        if(0>1){
+            System.out.print("It worked!");
+        }
+    }
+}
---
+++ src/testTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jason on 7/22/2016.
+ */
+
+public class testTest {
+}
